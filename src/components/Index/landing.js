d68bcc3ff777a9281be859019c22748bb06bca4d
Tolu Akindele <tolu@akindele.ca>
2020-10-09 11:21:27 -0700

    Add waves to landing page

--- src/components/Index/landing.js
+++ src/components/Index/landing.js
@@ -14,9 +14,18 @@ const bounce = keyframes`
   }
 `
 
+const animateWave = keyframes`
+  0% {
+    transform: scale(1,0);
+  }
+  100% {
+    transform: scale(1,1);
+  }
+`
+
 const Title = styled.h1`
   margin: 0 0 0.25em 0;
-  color: #feb80a;
+  color: #000000;
   font-size: 5em;
   font-weight: 800;
 
@@ -29,14 +38,14 @@ const Title = styled.h1`
 `
 
 const Container = styled.header`
-  background-color: #141621;
+  background-color: #feb80a;
   height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
   position: relative;
   text-align: center;
-  background-image: url(${background});
+  // background-image: url(${background});
   background-position: center;
   background-size: cover;
   flex-direction: column;
@@ -44,7 +53,7 @@ const Container = styled.header`
   &::after {
     content: "⌄";
     font-weight: 100;
-    color: #feb80a;
+    color: #000000;
     position: absolute;
     opacity: 0.8;
     text-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.5);
@@ -62,7 +71,7 @@ const Container = styled.header`
 `
 
 const Subtitle = styled.h2`
-  color: #feb80a;
+  color: #000000;
   font-size: 1.5em;
   font-weight: 400;
   max-width: 800px;
@@ -72,6 +81,13 @@ const Subtitle = styled.h2`
   @media (max-width: 750px) {
     font-size: 1em;
   }
+`
+const Transition = styled.svg`
+  display: block;
+  position: absolute;
+  bottom: 0rem;
+  transform-origin: bottom;
+  animation: ${animateWave} 1000ms cubic-bezier(0.23, 1, 0.32, 1) forwards;
 `
 
 const Landing = () => {
@@ -96,6 +112,7 @@ const Landing = () => {
       </div> */}
       <Title>The Last Train Podcast</Title>
       <Subtitle>{siteMetadata.description}</Subtitle>
+      <Transition xmlns="http://www.w3.org/2000/svg" viewBox="0 -60 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,256L48,218.7C96,181,192,107,288,106.7C384,107,480,181,576,176C672,171,768,85,864,48C960,11,1056,21,1152,37.3C1248,53,1344,75,1392,85.3L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></Transition>
     </Container>
   )
 }
