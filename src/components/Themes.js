e9a096be67e9fbb1c734393e3cdb0030f9a9fedd
Tolu Akindele <tolu@akindele.ca>
2021-02-17 00:18:11 -1000

    Dark mode button

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -4,12 +4,14 @@ export const lightTheme = {
   body: brandColors.brandYellow,
   text: brandColors.brandBlack,
   secondaryBackground: '#999',
-  // toggleBorder: '#FFF',
+  background: brandColors.brandWhite,
+  toggleBorder: brandColors.brandBlack,
 
 }
 export const darkTheme = {
   body: brandColors.brandBlack,
   text: brandColors.brandWhite,
   secondaryBackground: '#363537',
-  // toggleBorder: '#6B8096',
+  background: brandColors.brandYellow,
+  toggleBorder: brandColors.brandWhite,
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,21 +7,23 @@ import Landing from "../components/Index/landing"
 import Podcast from "../components/Index/podcast"
 import Hosts from "../components/Index/hosts"
 
+import Toggle from "../components/Toggler";
 import { ThemeProvider } from "styled-components";
+import { useDarkMode } from "../components/useDarkMode";
 import { GlobalStyles } from "../components/globalStyles";
 import { lightTheme, darkTheme } from "../components/Themes";
 
 const IndexPage = () => {
-  const [theme, setTheme] = useState('light');
-  const themeToggler = () => {
-    theme === 'light' ? setTheme('dark') : setTheme('light')
-  }
+  const [theme, themeToggler, mountedComponent] = useDarkMode();
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
+  if(!mountedComponent) return <div/>
 
   return (
-    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+    <ThemeProvider theme={themeMode}>
       <GlobalStyles/>
       <Layout>
-        <button onClick={themeToggler}>Switch Theme</button>
+        <Toggle theme={theme} toggleTheme={themeToggler} />
         <SEO title="The Last Train Podcast" />
         <Landing />
         <Podcast />
