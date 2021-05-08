3d7a39341bad18cebc12a0c75047bcf43c9c8b6b
Tolu Akindele <tolu@akindele.ca>
2021-05-07 19:09:00 -0700

    Update src/components/layout.js & src/hooks/useScrollDirection.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,27 +2,14 @@ import React from "react"
 import "./layout.css"
 import Footer from "./footer"
 import Navbar from "./Navbar/Navbar"
-import styled, { css } from "styled-components"
-
-const StyledLayout = styled.div`
-  position: unset;
-
-  ${(props) =>
-    props.navbarOpen &&
-    css`
-      position: fixed;
-    `};
-`
-
-// const navbarOpen = false
 
 const Layout = (props) => {
   return (
-    <StyledLayout>
+    <>
       <Navbar isHome={props.isHome} />
       {props.children}
       <Footer />
-    </StyledLayout>
+    </>
   )
 }
 
